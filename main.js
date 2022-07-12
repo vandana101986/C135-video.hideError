450d031706d900da48acc74acb244b8891d2d9a4
vandana101986 <73528385+vandana101986@users.noreply.github.com>
2022-07-12 12:58:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+//status = "";
+function preload()
+{
+    video = createVideo("video.mp4");
+    video.hide();
+}
+function setup()
+{
+canvas = createCanvas(450, 350);
+canvas.center();
+
+}
+function draw()
+{
+image(video, 0, 0, 450, 350);
+}
+
+
+/*function start()
+{
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+function modelLoaded()
+{
+    console.log("Model Loaded");
+    status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}*/
